34467f58e55cd249240642ace2caf25a65ecceac
arvin-sys <57642837+arvin-sys@users.noreply.github.com>
2019-11-21 20:37:00 +0100

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+    multiplyMatrices: function(x, y) {
+
+    let xRow = x.length;
+    let xCol = x[0].length;
+    let yRow = y.length;
+    let yCol = y[0].length;
+
+    if(xCol != yRow){
+        return null;
+    }
+
+    let mat = new Array(xRow);   
+
+    for(let i = 0; i < xRow; i++) {
+
+        mat[i] = new Array(yCol);             
+        for(let j = 0; j < yCol; j++) {
+
+            mat[i][j] = 0;                    
+            for(let k = 0; k < xCol; k++) {
+
+                mat[i][j] += x[i][k] * y[k][j];      
+            }
+        }
+    }
+    return mat;      
+}
+}
