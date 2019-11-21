2f61480213f3bef115538a18a56293564f7c665c
arvin-sys <57642837+arvin-sys@users.noreply.github.com>
2019-11-21 23:45:45 +0400

    Make improvements

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const p = require("./matrix.js");
 const m = require("./utils.js");
 
 const matrix1 = [[4, 3], [1, 6], [9, 2]],
-
       matrix2 = [[1, 1, 1], [7, 2, 9]];
        
 const result = m.multiplyMatrices(matrix1, matrix2);
-
 p.printMatrix(result);
