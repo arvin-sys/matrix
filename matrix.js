3a1fbcd1e316a81e30ba1459b1d00a3f2ae3c5b6
arvin-sys <57642837+arvin-sys@users.noreply.github.com>
2019-11-21 23:47:14 +0400

    Make improvements

--- matrix.js
+++ matrix.js
@@ -3,7 +3,9 @@ module.exports = {
     printMatrix: function(matrix) {
 
         for(let i = 0; i < matrix.length; i++) {
+            
             let s = "";
+            
             for(let j = 0; j < matrix[i].length; j++) {
                 s = s + matrix[i][j] + " ";
             }
